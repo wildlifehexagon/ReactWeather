1c3298d1ac5e0c3b27d842a72ccb333da0e2093d
wildlifehexagon <crossthebreeze@gmail.com>
2017-06-16 12:52:11 -0500

    added examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function() {
@@ -10,7 +11,18 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Chicago">Chicago, IL</Link>
+        </li>
+        <li>
+          <Link to="/?location=Reykjavik">Reykjavik, Iceland</Link>
+        </li>
+      </ol>
+    </div>
   )
 };
 
