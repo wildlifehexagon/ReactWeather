2faf05efbc8ba6dfe6c7878c1d44f1b9b4889678
wildlifehexagon <crossthebreeze@gmail.com>
2017-06-16 13:20:44 -0500

    added content to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,19 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This is the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built on React. I have built this for The Complete React Web App Developer Course. Will be customized soon.</p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - this was the JavaScript framework used
+        </li>
+        <li>
+          <a href="http://www.openweathermap.org">OpenWeatherMap</a> - this is the weather API
+        </li>
+      </ul>
     </div>
   )
 };
